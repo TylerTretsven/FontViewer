fdc52f0e141ed3061b0b9d44846789162f49fe80
Tyler Tretsven <tyler.tretsven@gmail.com>
2013-11-04 18:47:50 -0600

    Removed User Route

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -28,8 +27,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
